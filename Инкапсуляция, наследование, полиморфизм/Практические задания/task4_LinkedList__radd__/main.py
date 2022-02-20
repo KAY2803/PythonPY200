aa03fb66768533007c0b285b067199b2839eb243
Anna Kim <kimspb85@mail.ru>
2022-02-20 21:51:52 +0300

    o_notation_ll

--- Инкапсуляция, наследование, полиморфизм/Практические задания/task4_LinkedList__radd__/main.py
+++ Инкапсуляция, наследование, полиморфизм/Практические задания/task4_LinkedList__radd__/main.py
@@ -4,43 +4,50 @@ from node import Node
 
 
 class LinkedList:
-    def __init__(self, data: Iterable = None):
+    def __init__(self, data: Iterable = None):  # O(n)
         """Конструктор связного списка"""
-        self.len = 0
-        self.head: Optional[Node] = None
-        self.tail = self.head
+        self.len = 0    # O(1)
+        self.head: Optional[Node] = None    # O(1)
+        self.tail = self.head   # O(1)
 
-        if data is not None:
-            for value in data:
-                self.append(value)
+        # if data is not None:
+        #     for value in data:  # O(n)
+        #         self.append(value)  # O(1)
 
-    def append(self, value: Any):
+        if not self and data is not None:
+            self = data     # O(1)
+
+        if self and data is not None:
+            for value in data:     # O(n)
+                self.append(value)  # O(1)
+
+    def append(self, value: Any):   # O(1)
         """ Добавление элемента в конец связного списка. """
-        append_node = Node(value)
+        append_node = Node(value)   # O(1)
 
         if self.head is None:
-            self.head = self.tail = append_node
+            self.head = self.tail = append_node # O(1)
         else:
-            self.linked_nodes(self.tail, append_node)
-            self.tail = append_node
+            self.linked_nodes(self.tail, append_node)   # O(1)
+            self.tail = append_node     # O(1)
 
         self.len += 1
 
-    def step_by_step_on_nodes(self, index: int) -> Node:
+    def step_by_step_on_nodes(self, index: int) -> Node:    # O(n)
         """ Функция выполняет перемещение по узлам до указанного индекса. И возвращает узел. """
-        if not isinstance(index, int):
+        if not isinstance(index, int):  # O(1)
             raise TypeError()
 
-        if not 0 <= index < self.len:  # для for
+        if not 0 <= index < self.len:   # O(1)
             raise IndexError()
 
-        current_node = self.head
-        for _ in range(index):
-            current_node = current_node.next
+        current_node = self.head    # O(1)
+        for _ in range(index):  # O(n)
+            current_node = current_node.next    # O(1)
 
         return current_node
 
-    @staticmethod
+    @staticmethod   # O(1)
     def linked_nodes(left_node: Node, right_node: Optional[Node] = None) -> None:
         """
         Функция, которая связывает между собой два узла.
@@ -48,19 +55,19 @@ class LinkedList:
         :param left_node: Левый или предыдущий узел
         :param right_node: Правый или следующий узел
         """
-        left_node.next = right_node
+        left_node.next = right_node # O(1)
 
-    def __getitem__(self, index: int) -> Any:
+    def __getitem__(self, index: int) -> Any:   # O(n)
         """ Метод возвращает значение узла по указанному индексу. """
-        node = self.step_by_step_on_nodes(index)
+        node = self.step_by_step_on_nodes(index)    # O(n)
         return node.value
 
-    def __setitem__(self, index: int, value: Any) -> None:
+    def __setitem__(self, index: int, value: Any) -> None:  # O(n)
         """ Метод устанавливает значение узла по указанному индексу. """
-        node = self.step_by_step_on_nodes(index)
+        node = self.step_by_step_on_nodes(index)    # O(n)
         node.value = value
 
-    def to_list(self) -> list:
+    def to_list(self) -> list:  # O(n)
         return [linked_list_value for linked_list_value in self]
 
     def __repr__(self) -> str:
@@ -69,18 +76,40 @@ class LinkedList:
     def __str__(self) -> str:
         return f"{self.to_list()}"
 
-    def __add__(self, other: ["LinkedList", list]) -> "LinkedList":
-        if not isinstance(other, (LinkedList, list)):
+    def insert(self, index: int, value: Any) -> None:   # O(n)
+        #реализовать алгоритм вставки элемента
+        if not isinstance(index, int):     # O(1)
+            raise TypeError()
+        if not 0 <= index:  # O(1)
+            raise IndexError()
+
+        new_node = Node(value)  # O(1)
+
+        if index == 0:
+            new_node.next = self.head   # O(1)
+            self.head = new_node    # O(1)
+            self.len += 1
+        elif index > self.len:
+            self.append(value)  # O(1)
+        else:
+            prev_node = self.step_by_step_on_nodes(index-1) # O(n)
+            next_node = prev_node.next  # O(1)
+            self.linked_nodes(prev_node, new_node)  # O(1)
+            self.linked_nodes(new_node, next_node)  # O(1)
+            self.len += 1
+
+    def __add__(self, other: ["LinkedList", list]) -> "LinkedList":     # O(n)
+        if not isinstance(other, (LinkedList, list)):   # O(1)
             raise TypeError
 
-        for item in other:
-            self.append(item)
+        for item in other:  # O(n)
+            self.append(item)   # O(1)
 
         return self
 
     # определить метод сложения, когда LinkedList находится справа от оператора сложения
     def __radd__(self, other: list):
-        return LinkedList(other) + self
+        return LinkedList(other + self.to_list())   # O(n)
 
 if __name__ == "__main__":
     ll = LinkedList()
